e358476efc0ad5e61951fc2933cdccad4f8329e5
mehulmepani <mehul_mep@hotmail.com>
2018-08-08 09:59:09 +0100

    hashmap

--- Account_Management/src/account.java
+++ Account_Management/src/account.java
@@ -1,9 +1,9 @@
 
 public class account {
 	
-	String firstName;
-	String lastName;
-	int accountNumber;
+	private String firstName;
+	private String lastName;
+	private int accountNumber;
 
 	public account (String firstName, String lastName, int accountNumber) {
 		this.firstName = firstName;
@@ -11,55 +11,32 @@ public class account {
 		this.accountNumber = accountNumber;
 	}
 	
-	
-
-	
 	public String getFirstName() {
 		return firstName;
 	}
-
-
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
-
-
 	public String getLastName() {
 		return lastName;
 	}
-
-
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
-
-
 	public int getAccountNumber() {
 		return accountNumber;
 	}
-
-
-
 
 	public void setAccountNumber(int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 
-
-
-
 	public String toString() {
-		return "account [firstName=" + firstName + ", lastName=" + lastName + ", accountNumber=" + accountNumber + "]";
+		return " [firstName = " + firstName + ", lastName = " + lastName + ", accountNumber = " + accountNumber + "]";
 	}
-	
-	
 	
 }
