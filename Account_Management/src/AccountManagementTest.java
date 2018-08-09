111cd834d2018766298f0bdc7dd249b6fe96afb9
mehulmepani <mehul_mep@hotmail.com>
2018-08-09 09:17:16 +0100

    red

--- Account_Management/src/AccountManagementTest.java
+++ Account_Management/src/AccountManagementTest.java
@@ -9,10 +9,23 @@ public class AccountManagementTest {
 	@Test
 	public void test1() {
 		account accfive = new account("dave", "smith", 1234);
-		
+		account accsix = new account("dave", "tom", 6789);
 		service service = new service();
 		
 		service.addAccount(accfive);
+		service.addAccount(accsix);
+		
+		// use while loop 
+		
+		assertEquals(2, service.cyclingThroughHash());
+		// create a method that will have a string parameter
+		
+		//while loop to loop through hash if it has anymore entry
+		//for loop to loop through the hash checking the value
+		//if statement to check the values passed on by the parameter exists
+		//if it exist, add a counter to count them
+		
+		
 	}
 
 }
